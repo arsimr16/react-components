1615b332e1b514589aa392233f2e51cbafd6b259
arsimr16 <arsimr16@g.holycross.edu>
2017-09-08 11:58:06 -0700

    finished making grocery list

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-// TODO
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map((item, i) => 
+      <GroceryListItem key={i} item={item} />
+    )}
+  </ul>
+);
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+  	super(props);
+
+  	this.state = {
+  		hover: false
+  	};
+  }
+
+  onListItemHover() {
+  	this.setState({
+  		hover: !this.state.hover
+  	});
+  }
+
+  render() {
+    var style = {
+    	fontWeight: this.state.hover ? 'bold' : 'normal'
+    };
+
+  	return (
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.item}</li>
+  	);
+  }
+}
+
+ReactDOM.render(<GroceryList items={['cucumber', 'kale', 'ice cream']}/>, document.getElementById('app'));
